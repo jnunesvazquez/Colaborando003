2dbba51faad04a2f9c8e131690a8ff13ec338403
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2021-01-29 09:17:46 +0100

    Trabajando rama colaborador

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Primera funcionalidad acabada");
+        System.out.println("Segunda funcionalidade acabada.");
+
     }
 }
