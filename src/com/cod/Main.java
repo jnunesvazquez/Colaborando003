5529f98e8a8e29eae4540e0ba49be5a9e17737a2
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-01-29 09:05:02 +0100

    Primera funcionalidad acabada

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -3,6 +3,6 @@ package com.cod;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Primera funcionalidad acabada");
     }
 }
